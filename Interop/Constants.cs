249d0197f2ae3a30049ea91d838accc02916cd33
Nanov <124323250+isolateV@users.noreply.github.com>
2024-03-19 02:16:04 +0900

    s

--- Interop/Constants.cs
+++ Interop/Constants.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
 
 namespace TesseractDotnetWrapper.Interop
 {
@@ -7,16 +7,41 @@ namespace TesseractDotnetWrapper.Interop
     /// </summary>
     internal static class Constants
     {
-#if WINDOWS
         public const string LeptonicaDllName = "tesseract/win_x64/leptonica-1.84.1.dll";
         public const string TesseractDllName = "tesseract/win_x64/tesseract53.dll";
-#elif MACOS
+
+        /*
         public const string LeptonicaDllName = "tesseract/macos_aarch64/libleptonica.6.0.0.dylib";
         public const string TesseractDllName = "tesseract/macos_aarch64/libtesseract.5.3.4.dylib";
-# else
-        public const string LeptonicaDllName = "tesseract/linux_x64/libleptonica.6.0.0.dylib";
-        public const string TesseractDllName = "tesseract/linux_x64/libtesseract.5.3.4.dylib";
-#endif
+        */
+        /*
+        public const string LeptonicaDllName = "tesseract/win_x64/leptonica-1.84.1.dll";
+        public const string TesseractDllName = "tesseract/win_x64/tesseract53.dll";
+        */
+
+
+        /*
+        static Constants()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                LeptonicaDllName = "tesseract/macos_aarch64/libleptonica.6.0.0.dylib";
+                TesseractDllName = "tesseract/macos_aarch64/libtesseract.5.3.4.dylib";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                LeptonicaDllName = "tesseract/macos_aarch64/libleptonica.6.0.0.dylib";
+                TesseractDllName = "tesseract/macos_aarch64/libtesseract.5.3.4.dylib";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                LeptonicaDllName = "tesseract/macos_aarch64/libleptonica.6.0.0.dylib";
+                TesseractDllName = "tesseract/macos_aarch64/libtesseract.5.3.4.dylib";
+            }
+        }
+        */
+
+
 
         // tesseract uses an int to represent true false values.
         public const int TRUE = 1;
