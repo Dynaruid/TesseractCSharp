7c2494f8b87c2df969b9ac3c0c0f95e3863e98bb
ceramic <ceramic>
2024-03-22 20:07:37 +0900

    m

--- src/TesseractDotnetWrapper/Interop/NativeConstants.cs
+++ src/TesseractDotnetWrapper/Interop/NativeConstants.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
 
 namespace TesseractDotnetWrapper.Interop
 {
@@ -11,9 +11,9 @@ namespace TesseractDotnetWrapper.Interop
         public const string TesseractWinX64DllName = "tesseract/win_x64/tesseract53.dll";
 
         public const string LeptonicaMacosAArch64DllName =
-            "tesseract/macos_aarch64/libleptonica.6.0.0";
+            "tesseract/macos_aarch64/libleptonica.6.0.0.dylib";
         public const string TesseractMacosAArch64DllName =
-            "tesseract/macos_aarch64/libtesseract.5.3.4";
+            "tesseract/macos_aarch64/libtesseract.5.3.4.dylib";
         public const string LeptonicaLinuxX64DllName = "tesseract/linux_x64/libleptonica.so.6.0.0";
         public const string TesseractLinuxX64DllName = "tesseract/linux_x64/libtesseract.so.5.3.4";
 
